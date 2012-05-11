fa6d0251c0f9740a2ecbab494a8789506ecdf066
Paulo Favarato <paulofavarato@gmail.com>
2012-05-11 19:54:38 -0300

    Contem server, user, senha e nome do Banco de dados.

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Constantes do Banco de dados
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost", TRUE);
+defined('DB_USER')   ? null : define("DB_USER", "webservice");
+defined('DB_PASS')   ? null : define("DB_PASS", "minhasenha");
+defined('DB_NAME')   ? null : define("DB_NAME", "questoes");
+
+?>
